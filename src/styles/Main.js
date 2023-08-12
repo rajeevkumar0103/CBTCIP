bb6be9d892dd460726fdee63660b7340705b4e6a
Rajeev <yrajeev158@gmail.com>
2023-08-12 17:39:13 +0530

    first commit

---
+++ src/styles/Main.js
@@ -0,0 +1,59 @@
+// import { Component } from "react";
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: grid;
+  justify-content: center;
+  align-content: center;
+  width: 400px;
+  /* min-height: 500px; */
+  margin: 40px auto;
+  grid-template-columns: repeat(4, 100px);
+  grid-template-rows: minmax(100px, auto) repeat(4, 90px);
+  box-shadow: 2px 2px 10px #333;
+  border-radius: 10px;
+`;
+export const Screen = styled.div`
+  grid-column: 1/-1;
+  background-color: rgba(60, 64, 67, 0.75);
+  display: flex;
+  justify-content: space-around;
+  flex-direction: column;
+  padding: 15px;
+  word-wrap: break-word;
+  word-break: break-all;
+  text-align: right;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+`;
+
+export const Previous = styled.div`
+  color: rgba(255, 255, 255, 0.75);
+  font-size: 1.5rem;
+`;
+export const Current = styled.div`
+  color: white;
+  font-size: 2.5rem;
+`;
+
+export const Button = styled.button`
+  cursor: pointer;
+  font-size: 2rem;
+  padding: 30px;
+  outline: none;
+  border: 1px outset white;
+  background-color: rgba(255, 255, 255, 0.75);
+  &:hover {
+    background-color: rgba(255, 255, 255, 0.9);
+  }
+  ${function ({ gridSpan }) {
+    if (gridSpan) {
+      return `grid-column: span ${gridSpan}; `;
+    }
+  }};
+  ${({ operation }) => operation && `background-color: gray;`};
+  ${({ control }) => control && `background-color: orange;`};
+  ${({ equals }) =>
+    equals && `background-color: orange; border-bottom-right-radius:10px ;`};
+  ${({ decimal }) => decimal && ` border-bottom-left-radius:10px ;`};
+`;
